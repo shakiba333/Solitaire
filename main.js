2758a57cad61cc21f7718ccc4a50866f4da51c4d
shakiba333 <shakibaabdulrashid@gmail.com>
2023-06-06 10:20:58 -0400

    Refactored if statments

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
 // /*----- constants -----*/
-const data = ['A♠️', '2♠️', '3♠️', '4♠️', '5♠️', '6♠️', '7♠️', '8♠️', '9♠️', '10♠️', 'J♠️', 'Q♠️', 'K♠️',
+const cards = ['A♠️', '2♠️', '3♠️', '4♠️', '5♠️', '6♠️', '7♠️', '8♠️', '9♠️', '10♠️', 'J♠️', 'Q♠️', 'K♠️',
     'A♣️', '2♣️', '3♣️', '4♣️', '5♣️', '6♣️', '7♣️', '8♣️', '9♣️', '10♣️', 'J♣️', 'Q♣️', 'K♣️',
     'A♥️', '2♥️', '3♥️', '4♥️', '5♥️', '6♥️', '7♥️', '8♥️', '9♥️', '10♥️', 'J♥️', 'Q♥️', 'K♥️',
     'A♦️', '2♦️', '3♦️', '4♦️', '5♦️', '6♦️', '7♦️', '8♦️', '9♦️', '10♦️', 'J♦️', 'Q♦️', 'K♦️'];
+const containerPileIds = ['pile-7', 'pile-6', 'pile-5', 'pile-4', 'pile-3', 'pile-2', 'pile-1', 'deck'];
+
 
 // /*----- state variables -----*/
 
+let pile1 = [], pile2 = [], pile3 = [], pile4 = [], pile5 = [], pile6 = [], pile7 = [], deck = [];
 
 // /*----- cached elements  -----*/
-
-const deckEl = document.getElementById('deck');
-
-
-const pile7El = document.getElementById('pile-7');
-const pile6El = document.getElementById('pile-6');
-const pile5El = document.getElementById('pile-5');
-const pile4El = document.getElementById('pile-4');
-const pile3El = document.getElementById('pile-3');
-const pile2El = document.getElementById('pile-2');
-const pile1El = document.getElementById('pile-1');
 
 
 // /*----- event listeners -----*/
 
 
 // /*----- functions -----*/
-function init() { }
+
 function shuffleCards(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -33,43 +25,39 @@ function shuffleCards(arr) {
     }
 }
 
-function allocatePiles() {
-    shuffleCards(data);
-    for (let i = data.length - 1; i > 0; i--) {
-        if (i <= 7) {
-            const div = document.createElement('div');
-            div.textContent = data[i];
-            pile7El.appendChild(div)
-        } else if (i > 7 && i < 14) {
-            const div = document.createElement('div');
-            div.textContent = data[i];
-            pile6El.appendChild(div);
-        } else if (i >= 14 && i < 19) {
-            const div = document.createElement('div');
-            div.textContent = data[i];
-            pile5El.appendChild(div);
-        } else if (i >= 19 && i < 23) {
-            const div = document.createElement('div');
-            div.textContent = data[i];
-            pile4El.appendChild(div);
-        } else if (i >= 23 && i < 26) {
-            const div = document.createElement('div');
-            div.textContent = data[i];
-            pile3El.appendChild(div);
-        } else if (i >= 26 && i < 28) {
-            const div = document.createElement('div');
-            div.textContent = data[i];
-            pile2El.appendChild(div);
-        } else if (i >= 28 && i < 29) {
-            const div = document.createElement('div');
-            div.textContent = data[i];
-            pile1El.appendChild(div);
-        } else {
-            const div = document.createElement('div');
-            div.textContent = data[i];
-            deckEl.appendChild(div);
+function createPiles() {
+    shuffleCards(cards);
+    pile1 = cards.slice(0, 7);
+    pile2 = cards.slice(7, 13);
+    pile3 = cards.slice(14, 19);
+    pile4 = cards.slice(19, 23);
+    pile5 = cards.slice(23, 26);
+    pile6 = cards.slice(26, 28);
+    pile7 = cards.slice(28, 29);
+    deck = cards.slice(29, 52);
+
+    const containerPileIds = ['pile-7', 'pile-6', 'pile-5', 'pile-4', 'pile-3', 'pile-2', 'pile-1', 'deck'];
+
+    for (let i = 0; i < containerPileIds.length; i++) {
+        const currentPile = [pile1, pile2, pile3, pile4, pile5, pile6, pile7, deck][i];
+        const currentContainerPileId = containerPileIds[i];
+
+        const currentPileContainer = document.getElementById(currentContainerPileId);
+
+        for (let j = 0; j < currentPile.length; j++) {
+            const newCard = document.createElement('div');
+            newCard.textContent = currentPile[j];
+            currentPileContainer.appendChild(newCard);
         }
     }
+    return [pile1, pile2, pile3, pile4, pile5, pile6, pile7, deck];
 }
-allocatePiles();
+
+createPiles();
+
+
+
+
+
+
 
