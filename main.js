fa28557606cc2f7b38c4ff927a2900f1011a9a09
shakiba333 <shakibaabdulrashid@gmail.com>
2023-06-08 22:46:16 -0400

    Added the rules of the game when moving elements

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+
+
+
+
+
+
+
+
+
 // /*----- constants -----*/
 const cards = ['A♠️', '2♠️', '3♠️', '4♠️', '5♠️', '6♠️', '7♠️', '8♠️', '9♠️', '10♠️', 'J♠️', 'Q♠️', 'K♠️',
     'A♣️', '2♣️', '3♣️', '4♣️', '5♣️', '6♣️', '7♣️', '8♣️', '9♣️', '10♣️', 'J♣️', 'Q♣️', 'K♣️',
@@ -5,11 +14,18 @@ const cards = ['A♠️', '2♠️', '3♠️', '4♠️', '5♠️', '6♠️',
     'A♦️', '2♦️', '3♦️', '4♦️', '5♦️', '6♦️', '7♦️', '8♦️', '9♦️', '10♦️', 'J♦️', 'Q♦️', 'K♦️'];
 
 
+
 // /*----- state variables -----*/
+let draggedCard = null;
+let droppedCard = null;
+let draggedCardText = null;
+let droppedCardText = null;
 
 
 // /*----- cached elements  -----*/
 const cardEls = document.getElementsByClassName('card');
+const piles = document.getElementsByClassName('piles');
+const foundationdationContainers = document.getElementsByClassName('foundationSlots');
 const pile1 = document.getElementById('pile1');
 const pile2 = document.getElementById('pile2');
 const pile3 = document.getElementById('pile3');
@@ -17,12 +33,26 @@ const pile4 = document.getElementById('pile4');
 const pile5 = document.getElementById('pile5');
 const pile6 = document.getElementById('pile6');
 const pile7 = document.getElementById('pile7');
+const found1 = document.getElementById('spade');
+const found2 = document.getElementById('clubs');
+const found3 = document.getElementById('hearts');
+const found4 = document.getElementById('diamonds');
+
 const PileContainers = [pile1, pile2, pile3, pile4, pile5, pile6, pile7];
+const foundationContainer = [found1, found2, found3, found4];
 
-// /*----- event listeners -----*/
 
+
+/*----- event listeners -----*/
+for (const foundationContainer of foundationdationContainers) {
+    foundationContainer.addEventListener('dragover', dragOver);
+    foundationContainer.addEventListener('drop', compareFoundationEl);
+}
+for (const pile of piles) {
+    pile.addEventListener('dragover', dragOver);
+    pile.addEventListener('drop', dragDrop);
+}
 for (const cardEl of cardEls) {
-
     cardEl.addEventListener('dragstart', dragStart);
     cardEl.addEventListener('dragover', dragOver);
     cardEl.addEventListener('dragenter', dragEnter);
@@ -30,9 +60,10 @@ for (const cardEl of cardEls) {
     cardEl.addEventListener('drop', dragDrop);
     cardEl.addEventListener('dragend', dragEnd);
 }
-// /*----- functions -----*/
 
+// /*----- functions -----*/
 allocateCards()
+
 function allocateCards() {
     shuffleCards(cards);
 
@@ -49,21 +80,119 @@ function shuffleCards(arr) {
         [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 }
+
 function checkLastChildInPile(containers) {
     containers.forEach(container => {
         const containerChildren = container.children;
         const lastChild = containerChildren[containerChildren.length - 1];
         if (lastChild) {
-            lastChild.classList.remove('hideCard');
-            lastChild.draggable = true;
+            faceUpLastChild(lastChild)
         }
     });
 }
+function faceUpLastChild(card) {
+    card.classList.remove('hideCard');
+    card.draggable = true;
 
+}
+
+function checkSequentialOrder(droppedCardText, draggedCardText) {
+    const droppedCardValue = droppedCardText.includes('A') ? '1' :
+        droppedCardText.includes('J') ? '11' :
+            droppedCardText.includes('Q') ? '12' :
+                droppedCardText.includes('K') ? '13' :
+                    droppedCardText;
+
+    const draggedCardValue = draggedCardText.includes('A') ? '1' :
+        draggedCardText.includes('J') ? '11' :
+            draggedCardText.includes('Q') ? '12' :
+                draggedCardText.includes('K') ? '13' :
+                    draggedCardText;
+
+    const droppedCardNumber = parseInt(droppedCardValue);
+    const draggedCardNumber = parseInt(draggedCardValue);
+
+    const isSequentialOrder = droppedCardNumber - draggedCardNumber === 1;
+
+    return isSequentialOrder;
+}
+
+function checkSequentialOrderForFoundation(droppedCardText, draggedCardText) {
+    const droppedCardValue = droppedCardText.includes('A') ? '1' :
+        droppedCardText.includes('J') ? '11' :
+            droppedCardText.includes('Q') ? '12' :
+                droppedCardText.includes('K') ? '13' :
+                    droppedCardText;
+
+    const draggedCardValue = draggedCardText.includes('A') ? '1' :
+        draggedCardText.includes('J') ? '11' :
+            draggedCardText.includes('Q') ? '12' :
+                draggedCardText.includes('K') ? '13' :
+                    draggedCardText;
+
+    const droppedCardNumber = parseInt(droppedCardValue);
+    const draggedCardNumber = parseInt(draggedCardValue);
+
+    const isSequentialOrder = draggedCardNumber - droppedCardNumber === 1;
+
+    return isSequentialOrder;
+}
+function compareFoundationEl(event) {
+    event.preventDefault();
+    droppedCard = event.target;
+    droppedCardText = droppedCard.innerText;
+    if ((droppedCard.innerText === '' && draggedCardText.includes('A'))) {
+        droppedCard.classList.remove('drag-over');
+        droppedCard.draggable = true;
+        droppedCard.appendChild(draggedCard);
+        console.log("Element dropped into pile container:", event.target.id);
+    } else if (checkFoundationRules(draggedCardText, droppedCardText)) {
+        droppedCard.classList.remove('drag-over');
+        droppedCard.appendChild(draggedCard);
+        console.log("Element dropped into pile container:", event.target.id);
+    }
+}
+
+function compareElements(event) {
+    if ((droppedCard.innerText === '' && draggedCardText.includes('K'))) {
+        droppedCard.classList.remove('drag-over');
+        droppedCard.appendChild(draggedCard);
+        console.log("Element dropped into pile container:", event.target.id);
+    } else if (checkPileRules(draggedCardText, droppedCardText)) {
+        droppedCard.classList.remove('drag-over');
+        droppedCard.appendChild(draggedCard);
+        console.log("Element dropped into pile container:", event.target.id);
+    }
+}
+
+
+function checkPileRules(draggedCardText, droppedCardText) {
+    return (
+        (draggedCardText.includes('♥️') && droppedCardText.includes('♣️') && checkSequentialOrder(droppedCardText, draggedCardText)) ||
+        (draggedCardText.includes('♣️') && droppedCardText.includes('♦️') && checkSequentialOrder(droppedCardText, draggedCardText)) ||
+        (draggedCardText.includes('♥️') && droppedCardText.includes('♠️') && checkSequentialOrder(droppedCardText, draggedCardText)) ||
+        (draggedCardText.includes('♣️') && droppedCardText.includes('♥️') && checkSequentialOrder(droppedCardText, draggedCardText)) ||
+        (draggedCardText.includes('♠️') && droppedCardText.includes('♥️') && checkSequentialOrder(droppedCardText, draggedCardText)) ||
+        (draggedCardText.includes('♠️') && droppedCardText.includes('♦️') && checkSequentialOrder(droppedCardText, draggedCardText)) ||
+        (draggedCardText.includes('♦️') && droppedCardText.includes('♣️') && checkSequentialOrder(droppedCardText, draggedCardText)) ||
+        (draggedCardText.includes('♦️') && droppedCardText.includes('♠️') && checkSequentialOrder(droppedCardText, draggedCardText))
+    );
+}
+
+function checkFoundationRules(draggedCardText, droppedCardText) {
+    return (
+        (draggedCardText.includes('♥️') && droppedCardText.includes('♥️')) && checkSequentialOrderForFoundation(droppedCardText, draggedCardText) ||
+        (draggedCardText.includes('♠️') && droppedCardText.includes('♠️')) && checkSequentialOrderForFoundation(droppedCardText, draggedCardText) ||
+        (draggedCardText.includes('♣️') && droppedCardText.includes('♣️')) && checkSequentialOrderForFoundation(droppedCardText, draggedCardText) ||
+        (draggedCardText.includes('♦️') && droppedCardText.includes('♦️')) && checkSequentialOrderForFoundation(droppedCardText, draggedCardText)
+
+    );
+}
 
 
 function dragStart(event) {
     draggedCard = event.target;
+    draggedCardText = draggedCard.innerText;
     event.dataTransfer.effectAllowed = 'move';
     event.target.classList.add('dragging');
 }
@@ -71,6 +200,7 @@ function dragOver(event) {
     event.preventDefault();
     event.dataTransfer.dropEffect = 'move';
 }
+
 
 function dragEnter(event) {
     event.target.classList.add('drag-over');
@@ -81,8 +211,9 @@ function dragLeave(event) {
 }
 function dragDrop(event) {
     event.preventDefault();
-    event.target.classList.remove('drag-over');
-    event.target.appendChild(draggedCard);
+    droppedCard = event.target;
+    droppedCardText = droppedCard.innerText;
+    compareElements(event);
 }
 
 function dragEnd(event) {
